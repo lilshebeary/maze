b854b30aaf32bac4e4e1a947e3bd8c397151de9a
Ciri Ryan <cirijaye@yahoo.com>
2022-01-25 05:32:43 -0800

    generate maze, horizontal and verticals

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const { 
-    Engine, 
-    Render, 
-    Runner, 
-    World, 
-    Bodies, 
-} =  Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -23,13 +18,28 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-
 // Walls
 const walls = [
   Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
   Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
   Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-  Bodies.rectangle(width, height / 2 , 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
 ];
 World.add(world, walls);
 
+// maze generation
+
+const grid = Array(cells)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+    .fill(null)
+    .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+
+
+ 
