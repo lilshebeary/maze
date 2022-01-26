b681b846f3e05172cdd57243197703829ec7960f
Ciri Ryan <cirijaye@yahoo.com>
2022-01-26 05:21:19 -0800

    add vertical walls, draw maze on world

--- index.js
+++ index.js
@@ -128,3 +128,22 @@ horizontals.forEach((row, rowIndex) => {
         World.add(world, wall);
     });
 });
+
+verticals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        }
+
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength,
+            rowIndex * unitLength + unitLength / 2,
+            1,
+            unitLength,
+            {
+                isStatic: true
+            }
+        );
+        World.add(world, wall);
+    });
+});
